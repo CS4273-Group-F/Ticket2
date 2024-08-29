66c23adf365e65f257b03955de055a1a8fd6c163
Kyumin Lee <2km4890@gmail.com>
2024-08-29 09:51:02 -0500

    updating content

--- demo-svelt-app/src/routes/+page.svelte
+++ demo-svelt-app/src/routes/+page.svelte
@@ -1,59 +1,28 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+	import { onMount } from 'svelte';
+  
+	onMount(() => {
+	  window.afdcStationsOptions = {
+		"country":"all",
+		"localeCountry":"US",
+		"path":"/find/route",
+		"query":{
+		  "fuel":["ELEC"],
+		  "ev_levels":["all"]
+		}
+	  };
+  
+	  const script = document.createElement('script');
+	  script.src = "https://widgets.nrel.gov/afdc/station-locator/assets/embed.js";
+	  script.async = true;
+	  script.defer = true;
+	  document.body.appendChild(script);
+	});
+  </script>
+  
+  <div id="afdc-stations">
+	<div id="afdc-stations-loading">Loading alternative fueling station locator...</div>
+  </div>
+  
+  <noscript>Please enable JavaScript to view the alternative fueling station locator.</noscript>
+  
