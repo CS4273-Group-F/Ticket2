dd84d97a3b0e43d7b0f89bab8ec907d47ecb669a
Kyumin Lee <2km4890@gmail.com>
2024-08-29 09:26:38 -0500

    updating title

--- demo-react-app_cra/src/App.js
+++ demo-react-app_cra/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Demo App</h1>
+      <h1>Demo React App</h1>
       <div id="afdc-stations">
         <div id="afdc-stations-loading">Loading alternative fueling station locator...</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
